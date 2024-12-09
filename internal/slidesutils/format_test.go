87bb3ff0cc178da267f741baf3dff7c58584b441
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2024-12-09 18:38:40 +0100

    feat

---
+++ internal/slidesutils/format_test.go
@@ -0,0 +1,28 @@
+package slidesutils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseContent(t *testing.T) {
+	input := `this is a **bold** word and this is a list:
+- the level of indentation should be 1
+  - this content should have a level indentation of 2
+and this is back to a level of indentation of zero`
+
+	expected := []chunk{
+		{content: "this is a ", isBold: false, indentationLevel: 0},
+		{content: "bold", isBold: true, indentationLevel: 0},
+		{content: " word and this is a list:", isBold: false, indentationLevel: 0},
+		{content: "the level of indentation should be 1", isBold: false, indentationLevel: 1},
+		{content: "this content should have a level indentation of 2", isBold: false, indentationLevel: 2},
+		{content: "and this is back to a level of indentation of zero", isBold: false, indentationLevel: 0},
+	}
+
+	result := parseContent(input)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("parseContent() = %+v, want %+v", result, expected)
+	}
+}
